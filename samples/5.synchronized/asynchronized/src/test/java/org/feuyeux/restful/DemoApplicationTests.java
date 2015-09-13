33223983a80bb8dac7407592eee6dea0a859ae9d
lu.hl <lu.hl@alibaba-inc.com>
2015-09-13 14:48:31 +0800

    update aync

--- samples/5.synchronized/asynchronized/src/test/java/org/feuyeux/restful/DemoApplicationTests.java
+++ samples/5.synchronized/asynchronized/src/test/java/org/feuyeux/restful/DemoApplicationTests.java
@@ -2,6 +2,7 @@ package org.feuyeux.restful;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.feuyeux.restful.domain.Books;
 import org.feuyeux.restful.web.AsyncResource;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -44,53 +45,43 @@ public class DemoApplicationTests {
 
     @Test
     public void testAsync() throws InterruptedException, ExecutionException {
+        final Invocation.Builder request = target("http://localhost:" + this.port + "/books").request();
+        final AsyncInvoker async = request.async();
+        final Future<Books> responseFuture = async.get(Books.class);
+        log.debug("First response time = " + System.currentTimeMillis());
+        Books result = null;
         try {
-            final Invocation.Builder request = target("http://localhost:" + this.port + "/books").request();
-            final AsyncInvoker async = request.async();
-            final Future<String> responseFuture = async.get(String.class);
-            log.debug("First response @" + System.currentTimeMillis());
-            String result = null;
-            try {
-                result = responseFuture.get(AsyncResource.TIMEOUT + 1, TimeUnit.SECONDS);
-            } catch (TimeoutException e) {
-                log.debug("%%%% " + e.getMessage());
-            } finally {
-                log.debug("Second response @" + System.currentTimeMillis());
-                log.debug("<<<<<<<<<<< book id array: " + result);
-            }
+            result = responseFuture.get(AsyncResource.TIMEOUT + 1, TimeUnit.SECONDS);
+        } catch (TimeoutException e) {
+            log.debug("", e);
         } finally {
-            log.debug("<-**Test Batch Save");
+            log.debug("Second response time = " + System.currentTimeMillis());
+            log.debug(result.getBookList().size());
         }
     }
 
     @Test
     public void testAsyncCallBack() throws InterruptedException, ExecutionException {
-        try {
-            final AsyncInvoker async = target("http://localhost:" + this.port + "/books").request().async();
-            final Future<String> responseFuture = async.get(new InvocationCallback<String>() {
-                @Override
-                public void completed(String result) {
-                    log.debug("On Completed: " + result);
-                }
-
-                @Override
-                public void failed(Throwable throwable) {
-                    log.debug("On Failed: " + throwable.getMessage());
-                    throwable.printStackTrace();
-                }
-            });
-            log.debug("First response time::" + System.currentTimeMillis());
-            String result = null;
-            try {
-                result = responseFuture.get(AsyncResource.TIMEOUT + 1, TimeUnit.SECONDS);
-            } catch (TimeoutException e) {
-                log.debug("%%%% " + e.getMessage());
-            } finally {
-                log.debug("Second response time::" + System.currentTimeMillis());
-                log.debug("<<<<<<<<<<< book id array: " + result);
+        final AsyncInvoker async = target("http://localhost:" + this.port + "/books").request().async();
+        final Future<Books> responseFuture = async.get(new InvocationCallback<Books>() {
+            @Override
+            public void completed(Books result) {
+                log.debug("On Completed: " + result.getBookList().size());
             }
+
+            @Override
+            public void failed(Throwable throwable) {
+                log.debug("On Failed: " + throwable.getMessage());
+                throwable.printStackTrace();
+            }
+        });
+        log.debug("First response time::" + System.currentTimeMillis());
+        try {
+            responseFuture.get(AsyncResource.TIMEOUT + 1, TimeUnit.SECONDS);
+        } catch (TimeoutException e) {
+            log.debug("", e);
         } finally {
-            log.debug("testAsyncCallBack done.");
+            log.debug("Second response time::" + System.currentTimeMillis());
         }
     }
 
